c97407739d5f6db4675c40e72334934318aae466
Anthony Villena <quirinias@hotmail.com>
2012-04-01 12:09:18 -0500

    Add initial canvas drawing functions

--- study/serenity/serenity.js
+++ study/serenity/serenity.js
@@ -1,6 +1,15 @@
 $(function() {
+  
+  // Top level variables
   var ws = null;
   
+  // Check for websocket support
+  var support = "MozWebSocket" in window ? 'MozWebSocket' : ("WebSocket" in window ? 'WebSocket' : null);
+  if(support==null) {
+    $("#board").before("<p>Your browser is a little too old to run this application.</p>");
+  }
+  
+  // Set up networking 
   $('#connect').focus();
   $('#connect').click(function() {
     if (ws != null) {
@@ -10,7 +19,7 @@ $(function() {
     ws.onopen = function() {
       $('#log').append('<p>Connection Opened</p>');
     };
-    ws.onmessage = function (evt) { 
+    ws.onmessage = function (evt) {
       var received_msg = evt.data;
       $('#log').append('<p>Message Received: ' + received_msg + '</p>');
       
@@ -35,4 +44,41 @@ $(function() {
       ws = null;
     }
   });
+  
+  // Parsing example:
+  //cell = '{ "x" : 10, "y" : 10, "alive" : false }';
+  //obj = JSON.parse(cell);
+  
+  // Draw some stuff
+  var cells = new Array();
+  cells[0] = { x: 10, y: 10 }
+  cells[1] = { x: 20, y: 20 }
+  
+  function drawBoard() {
+    var board = document.getElementById("board");
+    var ctx = board.getContext("2d");
+    
+    for (var i = 0; i < cells.length; i++) {
+      cell = cells[i];
+      
+      var width = 3;
+      var x = cell.x * width;
+      var y = cell.y * width;
+      ctx.fillRect(x, y, width, width);
+    }
+  }
+  drawBoard();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
